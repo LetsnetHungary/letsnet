48e402b3c06ff3287f1e3a38cc89e996a2348117
hegelakos <hegel.akos@letsnet.hu>
2017-05-23 18:49:47 +0200

    categoryapi

---
+++ App/_controllers/RequestControllers/categoryapi.php
@@ -0,0 +1,15 @@
+<?php
+
+    class categoryapi extends CoreApp\InnerController {
+        public function __construct() {
+          parent::__construct(__CLASS__);
+          $this->a = $this->setAuthentication();
+          $this->a->userShouldChangeLocation("../AuthTest");
+          $this->model = $this->loadModel(__CLASS__);
+        }
+
+        public function getCategory() {
+          $p = $_POST;
+          $this->model->getCategory($p);
+        }
+    }
