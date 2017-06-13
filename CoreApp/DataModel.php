b2b48b65411001cfb756c28703ffb47b76be8109
Szilard Domotor <dom.szilard@gmail.com>
2017-06-13 18:22:33 +0200

    remove queryBuilder

--- CoreApp/DataModel.php
+++ CoreApp/DataModel.php
@@ -7,15 +7,12 @@ namespace CoreApp;
     protected $database;
     protected $PDO;
     protected $curl;
-    protected $queryBuilder;
     protected $log;
     protected $response;
 
     public function __construct() {
       $this->database = new Database();
-      $this->queryBuilder = new QueryBuilder();
       $this->log = array();
-      $this->response = new Response();
     }
 
     public function CURLWPOST($to, $url, $postarray) {
