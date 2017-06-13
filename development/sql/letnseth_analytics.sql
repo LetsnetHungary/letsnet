c1e1037f45591c2852901e259559046a2d78464e
Istvan Telek <telek96+github@gmail.com>
2017-06-13 18:10:20 +0200

    Analytics rewritten

---
+++ development/sql/letnseth_analytics.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `visits` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `uniquekey` varchar(10) NOT NULL,
+  `conntime` int(40) NOT NULL,
+  `connday` varchar(40) NOT NULL,
+  `url` varchar(100) NOT NULL,
+  `useragent` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=latin1;
